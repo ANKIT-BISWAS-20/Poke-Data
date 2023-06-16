c16748e30269d8600757374c8cdadc1e25934ec3
Ankit Biswas <ankit_b.cse2021@msit.edu.in>
2023-06-16 15:00:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+
+async function getpokemon(name){
+    let url = "https://pokeapi.co/api/v2/pokemon/"+String(name);
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    let pname = pokemon["name"];
+    let p_id = pokemon["id"];
+    let height = pokemon["height"];
+    let weight = pokemon["weight"];
+    let type = pokemon["types"][0]["type"]["name"];
+    let abilities = [pokemon["abilities"][0]["ability"]["name"],pokemon["abilities"][1]["ability"]["name"]];
+    let hp = pokemon["stats"][0]["base_stat"];
+    let attack = pokemon["stats"][1]["base_stat"];
+    let defence = pokemon["stats"][2]["base_stat"];
+    let special_attack = pokemon["stats"][3]["base_stat"];
+    let special_defence = pokemon["stats"][4]["base_stat"];
+    let speed = pokemon["stats"][5]["base_stat"];
+    const moves=[];
+    for(let i =0 ; i<8
+         ; i++){
+        moves[i] =pokemon["moves"][i]["move"]["name"];
+    }    
+    let img1 = pokemon["sprites"]["front_default"];
+    let img2 = pokemon["sprites"]["back_default"];
+
+    let exp = pokemon["base_experience"];
+
+    // console.log(pokemon);
+    // console.log(pname);
+    // console.log(p_id);
+    // console.log(height);
+    // console.log(weight);
+    // console.log(type);
+    // console.log(abilities);
+    // console.log(hp);
+    // console.log(attack);
+    // console.log(defence);
+    // console.log(special_attack);
+    // console.log(special_defence);
+    // console.log(speed);
+    // console.log(moves);
+    document.getElementById("name").innerHTML= pname;
+    document.getElementById("pid").innerHTML= p_id;
+    document.getElementById("type").innerHTML= type;
+    document.getElementById("height").innerHTML= height;
+    document.getElementById("weight").innerHTML= weight + String(" lbs");
+    document.getElementById("abilities").innerHTML= abilities;
+    document.getElementById("hp").innerHTML= hp;
+    document.getElementById("attack").innerHTML= attack;
+    document.getElementById("defence").innerHTML= defence;
+    document.getElementById("spatt").innerHTML= special_attack;
+    document.getElementById("spdef").innerHTML= special_defence;
+    document.getElementById("speed").innerHTML= speed;
+    document.getElementById("moves").innerHTML= moves;
+    document.getElementById("i1").src= img1;
+    document.getElementById("i2").src= img2;
+    document.getElementById("exp").innerHTML= exp;
+    
+    
+}
+
+
+
+
+function search_pokemon() {
+    let input = document.getElementById('sb').value;
+    input=input.toLowerCase();
+    getpokemon(input); 
+}
+
+function searchback(){
+    let ip1 = document.getElementById("pid").innerHTML;
+    ip1 = Number(ip1);
+    ip1 = ip1-1;
+    getpokemon(ip1);
+}
+
+function searchnext(){
+    let ip2 = document.getElementById("pid").innerHTML;
+    ip2 = Number(ip2);
+    ip2 = ip2+1;
+    getpokemon(ip2);
+}
+
+getpokemon(1);
+
+function contact(){
+    window.open("https://ankit-biswas-20.github.io/BISWAS-INDUSTRIES","_blank");
+}
